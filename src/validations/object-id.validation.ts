bd6f4cf418edcaa345916468115768a74e067f74
luiz2k <luizteles934@gmail.com>
2025-04-29 18:04:59 -0300

    feat: criar schema para validar id do mongodb

---
+++ src/validations/object-id.validation.ts
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import { z } from "zod";
+
+export const objectIdSchema = z
+  .string({
+    required_error: "Um id precisa ser informado",
+    invalid_type_error: "O id precisa ser do tipo string",
+  })
+  // Valida se o id é do tipo ObjectId do MongoDb
+  .refine((id) => mongoose.Types.ObjectId.isValid(id), {
+    message: "Informe um id no formato ObjectId do MongoDB",
+  });

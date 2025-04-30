7a4a21c85fe9925b4c7d675ee17b6b752bf099f4
luiz2k <luizteles934@gmail.com>
2025-04-30 11:55:06 -0300

    refactor: melhorar as menssagem de erro em createUserSchema

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -2,15 +2,31 @@ import { UserRole } from "src/schemas/user.schema";
 import { z } from "zod";
 
 export const createUserSchema = z.object({
-  email: z.string().email({ message: "E-mail inválido" }).toLowerCase(),
+  email: z
+    .string({
+      required_error: "E-mail obrigatório",
+      invalid_type_error: "O e-mail precisa ser uma string",
+    })
+    .email({ message: "Informe um e-mail válido" })
+    .toLowerCase(),
   password: z
-    .string()
+    .string({
+      required_error: "Senha obrigatória",
+      invalid_type_error: "A senha precisa ser uma string",
+    })
     .min(6, { message: "Senha muito curta, mínimo 6 caracteres" }),
   roles: z
-    .array(z.nativeEnum(UserRole, { message: "Papel inválido" }), {
-      message: "Esperado um array de papeis",
-    })
-    .min(1, { message: "Pelo menos um papel deve ser atribuído" }),
+    .array(
+      z.nativeEnum(UserRole, {
+        required_error: "Papel do usuário é obrigatório",
+        invalid_type_error: "Papel do usuário deve ser um valor válido",
+      }),
+      {
+        required_error: "A lista de papéis é obrigatória",
+        invalid_type_error: "A lista de papéis deve ser um array",
+      },
+    )
+    .min(1, { message: "Atribua pelo menos um papel ao usuário" }),
 });
 
 export type CreateUserDto = z.infer<typeof createUserSchema>;

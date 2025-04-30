2f47741a4b5fb1c0c65382fa23236cd11a7ba410
luiz2k <luizteles934@gmail.com>
2025-04-30 18:44:51 -0300

    feat: atualiza CreateCampaignDto para refletir CampaignSchema

--- src/campaign/dto/create-campaign.dto.ts
+++ src/campaign/dto/create-campaign.dto.ts
@@ -1,5 +1,4 @@
 import { CampaignStatus } from "src/schemas/campaign.schema";
-import { objectIdSchema } from "src/validations/object-id.validation";
 import { z } from "zod";
 
 export const createCampaignSchema = z.object({
@@ -14,14 +13,10 @@ export const createCampaignSchema = z.object({
   createdAt: z.date().optional(),
   updatedAt: z.date().optional(),
   finishedAt: z.date().nullable().optional(),
-  status: z.nativeEnum(CampaignStatus, {
-    required_error: "Status obrigatório",
-    invalid_type_error: "O status deve ser um valor válido",
-  }),
-  influencers: z
-    .array(objectIdSchema, {
-      required_error: "Influenciadores obrigatórios",
-      invalid_type_error: "Os influenciadores devem ser um array de ObjectIds",
+  status: z
+    .nativeEnum(CampaignStatus, {
+      required_error: "Status obrigatório",
+      invalid_type_error: "O status deve ser um valor válido",
     })
     .optional(),
 });

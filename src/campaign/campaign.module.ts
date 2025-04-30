c6b4ad699bf1c0ee4132c5d8462b9f1b5ac9a39f
luiz2k <luizteles934@gmail.com>
2025-04-30 15:33:48 -0300

    feat: importar CampaignSchema no module campaign

--- src/campaign/campaign.module.ts
+++ src/campaign/campaign.module.ts
@@ -1,8 +1,15 @@
 import { Module } from "@nestjs/common";
 import { CampaignController } from "./campaign.controller";
 import { CampaignService } from "./campaign.service";
+import { MongooseModule } from "@nestjs/mongoose";
+import { Campaign, CampaignSchema } from "src/schemas/campaign.schema";
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Campaign.name, schema: CampaignSchema },
+    ]),
+  ],
   controllers: [CampaignController],
   providers: [CampaignService],
 })

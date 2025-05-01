978fe8e2198e45ecfc0e9f322c80cf2feeb17e9c
luiz2k <luizteles934@gmail.com>
2025-05-01 00:29:38 -0300

    feat: adicionar método para criar participant

--- src/participant/participant.controller.ts
+++ src/participant/participant.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post, UseGuards } from "@nestjs/common";
+import { AuthGuard } from "src/auth/auth.guard";
+import { Roles } from "src/auth/roles.decorator";
+import { RolesGuard } from "src/auth/roles.guard";
+import { ZodValidationPipe } from "src/pipes/zod-validation.pipe";
+import { UserRole } from "src/schemas/user.schema";
+import {
+  CreateParticipantDto,
+  createParticipantSchema,
+} from "./dto/create-participant.dto";
+import { ParticipantService } from "./participant.service";
 
+@UseGuards(RolesGuard)
+@Roles(UserRole.ADMIN)
+@UseGuards(AuthGuard)
 @Controller("participant")
-export class ParticipantController {}
+export class ParticipantController {
+  constructor(private readonly participantService: ParticipantService) {}
+
+  @Post()
+  create(
+    @Body(new ZodValidationPipe(createParticipantSchema))
+    createParticipantDto: CreateParticipantDto,
+  ) {
+    const newParticipant = this.participantService.create(createParticipantDto);
+
+    return {
+      message: "Participante cadastrado com sucesso",
+      data: newParticipant,
+    };
+  }
+}
--- src/participant/participant.service.ts
+++ src/participant/participant.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from "@nestjs/common";
+import { ConflictException, Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { Participant } from "src/schemas/participant.schema";
+import { CreateParticipantDto } from "./dto/create-participant.dto";
 
 @Injectable()
 export class ParticipantService {
@@ -9,4 +10,15 @@ export class ParticipantService {
     @InjectModel(Participant.name)
     private readonly participantModel: Model<Participant>,
   ) {}
+
+  async create(createParticipantDto: CreateParticipantDto) {
+    const participantExists =
+      await this.participantModel.findOne(createParticipantDto);
+
+    if (participantExists) {
+      throw new ConflictException("Participante já cadastrado");
+    }
+
+    return await this.participantModel.create(createParticipantDto);
+  }
 }

c54829373ef8e5636ac10c5e73e7cc7e26c1a4cc
luiz2k <luizteles934@gmail.com>
2025-04-30 22:07:43 -0300

    feat: importar ParticipantSchema na module e service de participant

--- src/participant/participant.module.ts
+++ src/participant/participant.module.ts
@@ -1,8 +1,15 @@
 import { Module } from "@nestjs/common";
+import { Participant, ParticipantSchema } from "src/schemas/participant.schema";
 import { ParticipantController } from "./participant.controller";
 import { ParticipantService } from "./participant.service";
+import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Participant.name, schema: ParticipantSchema },
+    ]),
+  ],
   controllers: [ParticipantController],
   providers: [ParticipantService],
 })
--- src/participant/participant.service.ts
+++ src/participant/participant.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { Participant } from "src/schemas/participant.schema";
 
 @Injectable()
-export class ParticipantService {}
+export class ParticipantService {
+  constructor(
+    @InjectModel(Participant.name)
+    private readonly participantModel: Model<Participant>,
+  ) {}
+}
